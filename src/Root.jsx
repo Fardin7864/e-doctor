57b306bee8e658d38c043b245962b0e85773766b
Fardin7864 <fardinhasan1888@gmail.com>
2023-10-16 23:52:39 +0600

    add footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./components/Navbar/Nav";
+import Footer from "./components/Footer/Footer";
 
 const Root = () => {
     return (
         <div className=" font-open-sans">
             <Nav></Nav>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
